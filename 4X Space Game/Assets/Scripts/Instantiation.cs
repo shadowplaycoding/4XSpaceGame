ddd4c47e796124aa8eaf08ad7510c8366577bac1
Tom <shadowplaycoding@gmail.com>
2017-11-25 13:48:49 +1100

    Part 25 Changes

---
+++ 4X Space Game/Assets/Scripts/Instantiation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour {
+
+	public GameObject objectToInstantiate;
+
+	List<GameObject> objectsInstantiated = new List<GameObject>();
+
+	public void InstantiateObject()
+	{
+		GameObject GO = Instantiate(objectToInstantiate, this.transform);
+		objectsInstantiated.Add(GO);
+	}
+
+	public void RemoveObject(GameObject GO)
+	{
+		if (objectsInstantiated.Contains(GO))
+		{
+			objectsInstantiated.Remove(GO);
+		}
+	}
+}
---
+++ 4X Space Game/Assets/Scripts/UI/ClearBlueprintButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearBlueprintButton : MonoBehaviour {
+
+	Instantiation ins;
+
+	void Start()
+	{
+		ins = GetComponentInParent<Instantiation>();
+	}
+
+	public void DestroyObject()
+	{
+		ins.RemoveObject(this.gameObject);
+		Destroy(this.gameObject);
+	}
+}
